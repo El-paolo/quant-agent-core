6e2a28bfb5f7e9594d6f4d5cfbd8a6560cea3cf3
Paolo Ignacio Córdova Miranda <p.cordova@MacBook-Air-de-Paolo.local>
2026-02-18 18:00:22 -0300

    redefined the state, and added main folders for the agent structure

--- src/fina/agent/state.py
+++ src/fina/agent/state.py
@@ -1,10 +1,33 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Dict, Any
 
 @dataclass
 class AgentState:
+    #INPUTS
+    company: str
+    requested_series: List[str]
+    
+    ################
+    #RAW DATA
+    ################
     raw_inputs: List[str]
-    parsed_items: List[Dict[str, Any]]
-    signals: Dict[str, float]
-    insights: List[str]
-    confidence_score: float
+    raw_news: List[dict]
+    time_series_data: Dict[str, List[float]] = field(default_factory=dict)
+    ################
+    #PROCESSED DATA
+    ################
+    cleaned_series: Dict[str, Any] = field(default_factory=dict)
+    relevant_news: List[dict] = field(default_factory=list)
+    ################
+    #METRICS
+    ################
+    metrics: Dict[str, Any] = field(default_factory=dict)
+
+
+    ################
+    #NLP
+    ################
+    report_summary: str | None = None
+    sentiment_score: float
+    
+    
